03f51df503fd8cdeacfd35e3532faa89bd28a3a8
blancojohn <142819007+blancojohn@users.noreply.github.com>
2023-11-27 21:04:24 +0000

    Avances

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,8 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>"${variables.name} ${variables.lastName}"</h1>
+          <h3>"${variables.instagram}"</h3>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
